99a7f1d8f6ec680d14d6be576d8f767cc6238daa
zafar iqbal <68896404+Zafar-Iqbal-Khan@users.noreply.github.com>
2022-09-30 10:35:24 +0530

    first commit

--- server.js
+++ server.js
@@ -115,5 +115,9 @@ io.on("connection", (socket) => {
   });
 });
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Express!</h1>");
+});
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
